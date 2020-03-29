60171ef7baa02164ff7c21f9b77e127b6dc38794
andregord <andregord@ukr.net>
2020-03-30 00:49:28 +0300

    adding text to speach to train page and fixing bugs

--- MobileLabProject/MobileLabProject/Views/TrainWords.xaml.cs
+++ MobileLabProject/MobileLabProject/Views/TrainWords.xaml.cs
@@ -6,7 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,13 +18,14 @@ namespace MobileLabProject.Views
         ObservableCollection<Word> collection;
         ObservableCollection<Word> Sessioncollection;
         int points = 0;
+        bool isEnglish = false;
         public TrainWords()
         {
             InitializeComponent();
             
             collection = new ObservableCollection<Word>(App.Database.GetWords());
             Sessioncollection = new ObservableCollection<Word>(App.Database.GetWords());
-            ActualField.Text = Sessioncollection[0].RussianWord;
+            ActualField.Text = isEnglish==false?Sessioncollection[0].RussianWord:Sessioncollection[0].EnglishWord;
             
             
         }
@@ -33,21 +34,51 @@ namespace MobileLabProject.Views
 
         private void ResetBtn_Clicked(object sender, EventArgs e)
         {
+            points = 0;
+            collection = new ObservableCollection<Word>(App.Database.GetWords());
+            Sessioncollection = new ObservableCollection<Word>(App.Database.GetWords());
 
+            ActualField.Text = isEnglish == false ? Sessioncollection[0].RussianWord : Sessioncollection[0].EnglishWord;
+            answerFld.Text = "";
+            var tgr = new TapGestureRecognizer();
+            tgr.Tapped += async (s, ev) =>
+            {
+                var locales = await TextToSpeech.GetLocalesAsync();
+                SpeechOptions so;
+                if (isEnglish)
+                {
+                    so = new SpeechOptions { Locale = locales.FirstOrDefault(l => l.Country == "US") };
+                }
+                else
+                {
+                    so = new SpeechOptions { Locale = locales.FirstOrDefault(l => l.Country == "RU") };
+                }
+                await TextToSpeech.SpeakAsync(ActualField.Text, so);
+            };
+            ActualField.GestureRecognizers.Add(tgr);
         }
 
         private void ConfirmBtn_Clicked(object sender, EventArgs e)
         {
-            var currentWord = collection.FirstOrDefault(w => w.RussianWord == ActualField.Text);
-            if (currentWord.EnglishWord == answerFld.Text.Trim())
+            var currentWord = collection.FirstOrDefault(w => (isEnglish == false ? w.RussianWord : w.EnglishWord) == ActualField.Text);
+            if ((isEnglish==false?currentWord.EnglishWord:currentWord.RussianWord) == answerFld.Text.Trim())
             {
                 points++;
                 
 
             }
-            Sessioncollection.Remove(currentWord);
-            ActualField.Text = Sessioncollection.Count() == 0 ? $"{points}/{collection.Count()}" : Sessioncollection[0].RussianWord;
+            Sessioncollection.RemoveAt(0);
+            ActualField.Text = Sessioncollection.Count() == 0 ? $"{points}/{collection.Count()}" : isEnglish == false ? Sessioncollection[0].RussianWord : Sessioncollection[0].EnglishWord;
 
+        }
+
+        private void SwitchBtn_Clicked(object sender, EventArgs e)
+        {
+            ResetBtn_Clicked(this, null);
+            isEnglish = !isEnglish;
+            collection = new ObservableCollection<Word>(App.Database.GetWords());
+            Sessioncollection = new ObservableCollection<Word>(App.Database.GetWords());
+            ActualField.Text = isEnglish == false ? Sessioncollection[0].RussianWord : Sessioncollection[0].EnglishWord;
         }
     }
 }
